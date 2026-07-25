76d03f93dfeca4863861814ba8be74d49c78980b
kanduru-abhiram <kanduruajay820@gmail.com>
2026-07-26 00:15:09 +0530

    Added API using post and get

--- frontend/server/controller/customer.js
+++ frontend/server/controller/customer.js
@@ -1,21 +1,75 @@
-const Customer = require('../models/customer');
+// const Customer = require('../models/customer');
 
-exports.addnewcustomer = async (req, res) => {
+// exports.addnewcustomer = async (req, res) => {
+//     try {
+//         const { name, email, googleId, profilepicture } = req.body;
+//         // console.log(name,email,googleId,profilepicture)
+//         let exisitingcustomer = await Customer.findOne({ email: email }).lean().exec();
+//         if (exisitingcustomer) {
+//             res.send(exisitingcustomer)
+//         } else {
+//             const customer = new Customer({
+//                 name, email, googleId, profilepicture
+//             });
+//             const newCustomer = await customer.save()
+//             res.status(201).json(newCustomer);
+//         }
+//     } catch (error) {
+//         console.error('error adding customer', error);
+//         res.status(500).json({ error: "internal server error" });
+//     }
+// }
+
+
+
+// const Customer = require('../models/customer');
+
+// exports.addnewcustomer = async (req, res) => {
+//     try {
+//         const { name, email, googleId, profilepicture } = req.body;
+
+//         let existingcustomer = await Customer.findOne({ email: email }).lean().exec();
+
+//         if (existingcustomer) {
+//             res.send(existingcustomer);
+//         } else {
+//             const customer = new Customer({
+//                 name,
+//                 email,
+//                 googleId,
+//                 profilepicture
+//             });
+
+//             const newCustomer = await customer.save();
+//             res.status(201).json(newCustomer);
+//         }
+
+//     } catch (error) {
+//         console.error('error adding customer', error);
+//         res.status(500).json({ error: "internal server error" });
+//     }
+// };
+
+
+// Get customer profile
+exports.getCustomer = async (req, res) => {
     try {
-        const { name, email, googleId, profilepicture } = req.body;
-        // console.log(name,email,googleId,profilepicture)
-        let exisitingcustomer = await Customer.findOne({ email: email }).lean().exec();
-        if (exisitingcustomer) {
-            res.send(exisitingcustomer)
-        } else {
-            const customer = new Customer({
-                name, email, googleId, profilepicture
+        const { email } = req.query;
+
+        const customer = await Customer.findOne({ email: email });
+
+        if (!customer) {
+            return res.status(404).json({
+                message: "Customer not found"
             });
-            const newCustomer = await customer.save()
-            res.status(201).json(newCustomer);
         }
+
+        res.status(200).json(customer);
+
     } catch (error) {
-        console.error('error adding customer', error);
-        res.status(500).json({ error: "internal server error" });
+        console.error("Error fetching customer", error);
+        res.status(500).json({
+            error: "Internal server error"
+        });
     }
-}
+};
--- frontend/server/routes/customer.js
+++ frontend/server/routes/customer.js
@@ -1,7 +1,18 @@
-const express=require("express")
-const router=express.Router();
-const customercontroller=require("../controller/customer")
+const express = require("express")
+const router = express.Router();
+const customercontroller = require("../controller/customer")
 
-router.post("/customer",customercontroller.addnewcustomer)
+// Add new customer
+router.post("/customer", customercontroller.addnewcustomer)
 
-module.exports=router;
+// Get customer profile
+router.get("/", customercontroller.getCustomer)
+
+module.exports = router;
+// const express=require("express")
+// const router=express.Router();
+// const customercontroller=require("../controller/customer")
+
+// router.post("/customer",customercontroller.addnewcustomer)
+
+// module.exports=router;
