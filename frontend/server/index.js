6ddf6913ccfe798ecad7582791ee488888c6ff3d
kanduru-abhiram <kanduruajay820@gmail.com>
2026-07-25 22:22:18 +0530

    Deployed backend

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -29,7 +29,8 @@ app.get('/',(req,res)=>{
     res.send('Hello , Ted bus is working')
 })
 
-const PORT= 5000
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`)
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
